d6f6a7e050d78611465b0e3977c8b8be596f5599
Lozovyy™ <88165671+lozovyy@users.noreply.github.com>
2022-03-11 23:51:46 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+/* Testnet_Nosana_Phase_1 (!) */
+
 const simpleArray = [3, 5, 7, 15];
 const objectArray = [{ name: 'John' }, { name: 'Emma' }]
 
@@ -9,3 +11,31 @@ console.log( simpleArray.find(e => e === 10) )
 
 console.log( objectArray.find(e => e.name === 'John') )
 // expected output { name: 'John' }
+
+const inventory = [
+  {name: 'apples', quantity: 2},
+  {name: 'bananas', quantity: 0},
+  {name: 'cherries', quantity: 5}
+];
+
+function isCherries(fruit) {
+  return fruit.name === 'cherries';
+}
+
+console.log(inventory.find(isCherries)); // { name: 'cherries', quantity: 5 }
+
+/* find মেথড অলওয়েজ অ্যারের সরাসরি ভ্যালু রিটার্ণ করে। 
+অর্থাৎ কন্ডিশনের সাথে মিলে যাওয়ার পরে যে কারণে মিলসে সেই লজিক অনুযায়ী 
+ঐ অ্যারে থেকে প্রথম ভ্যালুটা রিটার্ণ করে। সে কারণে এখানে অ্যারের পুরো ভ্যালুটা আউটপুট হিসেবে দেখাচ্ছে। */
+
+// Same Code Using Arrow function & destructuring technique
+
+const inventory = [
+  {name: 'apples', quantity: 2},
+  {name: 'bananas', quantity: 0},
+  {name: 'cherries', quantity: 5}
+];
+
+const result = inventory.find( ({ name }) => name === 'cherries' );   // ({name})==> destructuring technique
+
+console.log(result); // { name: 'cherries', quantity: 5 }
